e5e127705e1355fa57d5cea72fd5f4f9c9664bea
web小菜鸡 <38740836+1131153523@users.noreply.github.com>
2019-06-09 10:50:05 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const resolve = dir => {
+  return path.join(__dirname, dir);
+};
+
+const BASE_URL = process.env.NODE_ENV === "production" ? "/" : "/";
+
+module.exports = {
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias.set("@", resolve("src"));
+  },
+  // 打包时不生成.map文件
+  prodcutionSourceMap: false,
+  // 跨域
+  devServer: {
+    proxy: "http://localhost:8080"
+  }
+};
